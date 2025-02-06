385d8dfd3ba7ff287245e8251ef95292b91caeab
WaltteriAXW <waltteri@axw.fi>
2025-02-06 11:28:24 +0200

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalCoffeeInterface from './components/GlobalCoffeeInterface';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <GlobalCoffeeInterface />
+    </div>
+  );
+}
+
+export default App;
